412ea5aa5f305ab95c7034eefa3c8fda45d9d5f3
fx-cyr <68352707+fx-cyr@users.noreply.github.com>
2020-12-23 22:14:16 -0500

    Added routing between menu and home

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,32 @@
 import React, { useState } from "react";
 import Home from "./Home";
 import Menu from "./Menu";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
   const [allTasks, setAllTasks] = useState([]);
 
-  return <Menu allTasks={allTasks} setAllTasks={setAllTasks} />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/menu">
+          <Menu />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
